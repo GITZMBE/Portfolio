845bfd58b685088bea97ce7445ece375c3fc3be6
GITZMBE <lucasfilip@hotmail.se>
2023-08-09 00:34:04 +0200

    responsive nav file

---
+++ src/components/Header/Responsive-nav/responsive-nav.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { AiOutlineClose, AiOutlineMail } from "react-icons/ai";
+import { BiLogoReact, BiLogoLinkedin, BiSolidContact } from "react-icons/bi";
+import { RxGithubLogo } from "react-icons/rx";
+
+function ResponsiveNav() {
+  const handleClick = () => {
+    const navContainer = document.getElementById("nav-container");
+    navContainer.classList.contains("hidden")
+      ? navContainer.classList.remove("hidden")
+      : navContainer.classList.add("hidden");
+  };
+
+  return (
+    <div
+      id='nav-container'
+      className='hidden fixed top-0 left-0 w-full h-screen bg-overPrimaryLight'
+    >
+      <nav className='flex flex-col justify-between gap-4 h-full w-[85%] sm:w-[65%] md:w-[55%] lg:w-[45%] py-8 px-4 bg-primaryLight transitioning dark:bg-primaryDark'>
+        <div>
+          <div className='flex justify-between items-center'>
+            <BiLogoReact
+              size={50}
+              className='fill-[#61DBFB] dark:fill-[#08B6CE]'
+            />
+            <a
+              href=''
+              className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
+            >
+              <AiOutlineClose
+                id='close-btn'
+                onClick={handleClick}
+                size={22}
+                className='cursor-pointer fill-overPrimaryLight hover:fill-dark dark:fill-lightOverPrimaryDark dark:hover:fill-darkOverPrimaryDark transioning'
+              />
+            </a>
+          </div>
+          <p className='text-xl dark:text-lightOverPrimaryDark px-2 py-4 border-b dark:border-b-darkOverPrimaryDark tracking-widest uppercase cursor-default'>
+            Contact for your own build
+          </p>
+          <ul className='flex flex-col gap-8 text-2xl py-4 list-none font-semibold'>
+            <li>
+              <a href='#home-container' target='_self'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href='#about-container' target='_self'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#skills-container' target='_self'>
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio-container' target='_self'>
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href='#contact-container' target='_self'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className='text-xl text-[#61DBFB] dark:text-[#08B6CE] cursor-default'>
+            Let's Connect!
+          </p>
+          <div className='flex justify-center gap-12 py-4'>
+            <a
+              href=''
+              className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
+            >
+              <RxGithubLogo size={22} className='' />
+            </a>
+            <a
+              href=''
+              className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
+            >
+              <BiLogoLinkedin size={22} />
+            </a>
+            <a
+              href=''
+              className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
+            >
+              <AiOutlineMail size={22} />
+            </a>
+            <a
+              href=''
+              className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
+            >
+              <BiSolidContact size={22} />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default ResponsiveNav;
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
-import { BiLogoReact, BiLogoLinkedin, BiSolidContact } from "react-icons/bi";
+import ResponsiveNav from "./Responsive-nav/responsive-nav";
+import { AiOutlineMenu } from "react-icons/ai";
 import { DiJsBadge } from "react-icons/di";
-import { RxGithubLogo } from "react-icons/rx";
 import DarkModeBtn from "./Mode/mode-btn";
 
 function Header() {
@@ -53,7 +52,6 @@ function Header() {
             <a
               href='#home-container'
               target='_self'
-              // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
             >
               Home
             </a>
@@ -62,7 +60,6 @@ function Header() {
             <a
               href='#about-container'
               target='_self'
-              // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
             >
               About
             </a>
@@ -71,7 +68,6 @@ function Header() {
             <a
               href='#skills-container'
               target='_self'
-              // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
             >
               Skills
             </a>
@@ -80,7 +76,6 @@ function Header() {
             <a
               href='#portfolio-container'
               target='_self'
-              // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
             >
               Portfolio
             </a>
@@ -89,7 +84,6 @@ function Header() {
             <a
               href='#contact-container'
               target='_self'
-              // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
             >
               Contact
             </a>
@@ -104,108 +98,7 @@ function Header() {
           className='fill-overPrimaryLight hover:fill-dark dark:fill-lightOverPrimaryDark dark:hover:fill-darkOverPrimaryDark transioning cursor-pointer lg:hidden'
         />
       </div>
-      <div
-        id='nav-container'
-        className='hidden fixed top-0 left-0 w-full h-screen bg-overPrimaryLight'
-      >
-        <nav className='flex flex-col justify-between gap-4 h-full w-[85%] sm:w-[65%] md:w-[55%] lg:w-[45%] py-8 px-4 bg-primaryLight transitioning dark:bg-primaryDark'>
-          <div>
-            <div className='flex justify-between items-center'>
-              <BiLogoReact
-                size={50}
-                className='fill-[#61DBFB] dark:fill-[#08B6CE]'
-              />
-              <AiOutlineClose
-                id='close-btn'
-                onClick={handleClick}
-                size={22}
-                className='cursor-pointer fill-overPrimaryLight hover:fill-dark dark:fill-lightOverPrimaryDark dark:hover:fill-darkOverPrimaryDark transioning'
-              />
-            </div>
-            <p className='text-xl dark:text-lightOverPrimaryDark px-2 py-4 border-b dark:border-b-darkOverPrimaryDark tracking-widest uppercase cursor-default'>
-              Contact for your own build
-            </p>
-            <ul className='flex flex-col gap-8 text-2xl py-4 list-none font-semibold'>
-              <li>
-                <a
-                  href='#home-container'
-                  target='_self'
-                  // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#about-container'
-                  target='_self'
-                  // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#skills-container'
-                  target='_self'
-                  // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
-                >
-                  Skills
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#portfolio-container'
-                  target='_self'
-                  // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
-                >
-                  Portfolio
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#contact-container'
-                  target='_self'
-                  // className='decoration-0 text-overPrimaryLight p-2 transitioning hover:text-dark hover:cursor-pointer dark:text-darkOverPrimaryDark dark:hover:text-lightOverPrimaryDark'
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className='text-xl text-[#61DBFB] dark:text-[#08B6CE] cursor-default'>
-              Let's Connect!
-            </p>
-            <div className='flex justify-center gap-12 py-4'>
-              <a
-                href=''
-                className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
-              >
-                <RxGithubLogo size={22} className='' />
-              </a>
-              <a
-                href=''
-                className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
-              >
-                <BiLogoLinkedin size={22} />
-              </a>
-              <a
-                href=''
-                className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
-              >
-                <AiOutlineMail size={22} />
-              </a>
-              <a 
-                href=''
-                className='p-4 rounded-full shadow-lg hover:shadow-overPrimaryLight cursor-pointer'
-              >
-                <BiSolidContact size={22} />
-              </a>
-            </div>
-          </div>
-        </nav>
-      </div>
+      <ResponsiveNav id='navContainer' />
     </header>
   );
 }
