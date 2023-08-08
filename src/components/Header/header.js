2c70741a19626dc45ebaef20eebe284217232e0a
GITZMBE <lucasfilip@hotmail.se>
2023-08-08 22:41:00 +0200

    react-icons

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { BiLogoReact } from "react-icons/bi";
+import { DiJsBadge } from "react-icons/di";
+import { RxGithubLogo } from "react-icons/rx";
 import DarkModeBtn from "./Mode/mode-btn";
 
 function Header() {
@@ -25,12 +28,23 @@ function Header() {
     boxShadow: "0 0 10px #000000" + shadowOpacity,
   };
 
+  const handleClick = () => {
+    const navContainer = document.getElementById("nav-container");
+    navContainer.classList.contains("hidden")
+      ? navContainer.classList.remove("hidden")
+      : navContainer.classList.add("hidden");
+  };
+
   return (
     <header
       style={headerStyle}
       className='fixed top-0 flex justify-between items-center w-full h-headerOffset py-0 px-4 sm:px-8 lg:px-20 bg-primaryLight transitioning z-1 dark:bg-primaryDark'
     >
-      <h1 className='text-dark text-2xl transitioning cursor-default dark:text-lightOverPrimaryDark'>
+      <h1 className='flex items-center gap-4 text-dark text-2xl transitioning cursor-default dark:text-lightOverPrimaryDark'>
+        <DiJsBadge
+          size={36}
+          className='fill-overSecondaryLight dark:fill-lightOverPrimaryDark'
+        /> 
         Portfolio
       </h1>
       <nav>
@@ -85,9 +99,26 @@ function Header() {
       <div className='flex gap-8'>
         <DarkModeBtn />
         <AiOutlineMenu
+          onClick={handleClick}
           size={22}
           className='fill-overPrimaryLight hover:fill-dark dark:fill-lightOverPrimaryDark dark:hover:fill-darkOverPrimaryDark transioning cursor-pointer lg:hidden'
         />
+      </div>
+      <div 
+        id='nav-container'
+        className='hidden fixed top-0 left-0 w-full h-screen bg-overPrimaryLight'
+      >
+        <nav className='flex flex-col gap-4 h-full w-[85%] sm:w-[65%] md:w-[55%] lg:w-[45%] py-8 px-4 bg-primaryLight transitioning dark:bg-primaryDark'>
+          <div className='flex justify-between items-center'>
+            <BiLogoReact size={50} fill='#336699' />
+            <AiOutlineClose
+              id='close-btn'
+              onClick={handleClick}
+              size={22}
+              className='cursor-pointer fill-overPrimaryLight hover:fill-dark dark:fill-lightOverPrimaryDark dark:hover:fill-darkOverPrimaryDark transioning'
+            />
+          </div>
+        </nav>
       </div>
     </header>
   );
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="grid place-items-center w-full h-36 bg-primaryLight text-darkOverPrimaryDark transitioning dark:bg-primaryDark cursor-default">
+    <footer className="grid place-items-center w-full py-12 px-4 sm:px-8 lg:px-12 bg-primaryLight transitioning dark:text-darkOverPrimaryDark dark:bg-primaryDark cursor-default">
         @LucasAndersson
     </footer>
   );
