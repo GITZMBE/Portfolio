61ae4fc0de94c05cdbd9e5c8ad6fbec1a02cb9cc
GITZMBE <lucasfilip@hotmail.se>
2023-08-10 00:19:04 +0200

    skill section responsive and style

--- src/components/Skills/Skill/skill.jsx
+++ src/components/Skills/Skill/skill.jsx
@@ -7,11 +7,11 @@ function Skill({ Icon, skill, color }) {
     //   <p className="">{skill}</p>
     // </div>
 
-    <div className='relative w-fit p-4 text-center text-dark transitioning rounded-xl overflow-hidden'>
+    <div className='relative w-fit min-w-[125px] sm:min-w-[150px] sm:min-h-[150px] lg:min-w-[200px] lg:min-h-[200px] p-4 text-center text-dark transitioning rounded-xl overflow-hidden hover:scale-105 cursor-pointer'>
       <div className="absolute top-0 left-0 w-full h-full bg-slate-100 dark:bg-slate-950 z-0 filter blur-lg backdrop-blur-lg opacity-30 dark:opacity-50 transitioning"></div>
-      <div className='flex flex-col gap-2 z-10 relative rounded-lg'>
+      <div className='flex flex-col h-full items-center justify-between gap-2 z-10 relative rounded-lg'>
         <Icon size={80} className={color} />
-        <p className="text-dark dark:text-light transitioning">{skill}</p>
+        <p className="lg:text-xl font-semibold text-dark dark:text-light transitioning">{skill}</p>
       </div>
     </div>
   )
