a6fc68c16a663ad36189dca6e2ed9c16e3f42965
GITZMBE <lucasfilip@hotmail.se>
2024-12-05 08:47:51 +0100

    Fix errors for unused vars

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,22 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['https://eu-west-2.graphassets.com', 'eu-west-2.graphassets.com', 'https://images.unsplash.com', 'images.unsplash.com', 'https://static.amazon.jobs', 'static.amazon.jobs'],
+    domains: [
+      'https://eu-west-2.graphassets.com', 
+      'eu-west-2.graphassets.com', 
+      'https://images.unsplash.com', 
+      'images.unsplash.com', 
+      'https://static.amazon.jobs', 
+      'static.amazon.jobs',
+      'https://cdn.jsdelivr.net',
+      'cdn.jsdelivr.net',
+      'https://cdn.worldvectorlogo.com',
+      'cdn.worldvectorlogo.com',
+      'https://cdn.icon-icons.com',
+      'cdn.icon-icons.com',
+      'https://upload.wikimedia.org',
+      'upload.wikimedia.org'
+    ],
   },
 };
 
